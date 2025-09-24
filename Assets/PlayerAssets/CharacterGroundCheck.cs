fa85ec9ce641eb0b359dc2a9ec3fee60b0ff7009
HongjunJo <hongjunjo23@gmail.com>
2025-09-24 23:07:10 +0900

    Assets: 산소통 추가

--- Assets/PlayerAssets/CharacterGroundCheck.cs
+++ Assets/PlayerAssets/CharacterGroundCheck.cs
@@ -16,8 +16,27 @@ public class CharacterGroundCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, groundLayer) || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, groundLayer);
+        Vector3 leftRayOrigin = transform.position + new Vector3(-colliderOffset.x, colliderOffset.y, colliderOffset.z);
+        Vector3 rightRayOrigin = transform.position + new Vector3(colliderOffset.x, colliderOffset.y, colliderOffset.z);
+
+        onGround = Physics2D.Raycast(rightRayOrigin, Vector2.down, groundLength, groundLayer) || Physics2D.Raycast(leftRayOrigin, Vector2.down, groundLength, groundLayer);
     }
 
     public bool GetOnGround() { return onGround; }
+
+    /// <summary>
+    /// Scene 뷰에서 땅 감지 범위를 시각적으로 표시합니다.
+    /// </summary>
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Vector3 position = transform.position;
+
+        Vector3 leftRayOrigin = position + new Vector3(-colliderOffset.x, colliderOffset.y, colliderOffset.z);
+        Vector3 rightRayOrigin = position + new Vector3(colliderOffset.x, colliderOffset.y, colliderOffset.z);
+
+        // 두 개의 Raycast를 시각화합니다.
+        Gizmos.DrawLine(rightRayOrigin, rightRayOrigin + Vector3.down * groundLength);
+        Gizmos.DrawLine(leftRayOrigin, leftRayOrigin + Vector3.down * groundLength);
+    }
 }
