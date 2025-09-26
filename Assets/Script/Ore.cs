99fb23ef28f6ae4c72ebc299d26a3384946f7558
ph605 <sangwoon605@gmail.com>
2025-09-26 13:54:12 +0900

    프리팹 수정 및 검으로 먹어지는거 수정

--- Assets/Script/Ore.cs
+++ Assets/Script/Ore.cs
@@ -1,10 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class Ore : MonoBehaviour
 {
     [SerializeField] Rigidbody2D[] OreBodies;
     [SerializeField] GameObject DropObject;
-     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float respawnTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
@@ -21,17 +23,43 @@ public class Ore : MonoBehaviour
         {
             OreBody.simulated = true;
         }
-        Invoke("DestroyThis", 1f);
+        StartCoroutine(nameof(RespawnCoroutine));
+        // Invoke(nameof(DestroyThis), 1f);
     }
 
     void DropOre()
     {
-        Instantiate(DropObject,transform.position - new Vector3(0,0.5f,0),Quaternion.identity);
+        Instantiate(DropObject, transform.position, Quaternion.identity);
     }
 
     void DestroyThis()
     {
         DropOre();
         Destroy(gameObject);
+    }
+    private IEnumerator RespawnCoroutine()
+    {
+        if (respawnTime > 0f)
+        {
+            DropOre();
+            // 시각적 오브젝트와 콜라이더를 비활성화합니다.
+            // gameObject.SetActive(false);
+            // GetComponent<Collider2D>().enabled = false; // 상호작용을 막기 위해 콜라이더도 끔
+            // 설정된 시간만큼 대기
+            yield return new WaitForSeconds(respawnTime);
+            Debug.Log("TEst");
+            ResetResource();
+        }
+
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    private void ResetResource()
+    {
+        // currentHp = maxHp;
+        // gameObject.SetActive(true);
+        // GetComponent<Collider2D>().enabled = true;
     }
 }
