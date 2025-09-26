29655b57d4b7928c82464bfc840d55d73bde7fc3
ph605 <sangwoon605@gmail.com>
2025-09-26 16:20:02 +0900

    광물 수정

--- Assets/Script/Ore.cs
+++ Assets/Script/Ore.cs
@@ -1,65 +1,93 @@
 using System.Collections;
+using System.ComponentModel;
 using UnityEngine;
 
 public class Ore : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D[] OreBodies;
+    [SerializeField] Transform target;
+    [SerializeField] GameObject[] OreBodies;
     [SerializeField] GameObject DropObject;
+    [SerializeField] private float bodyRemainTime;
     [SerializeField] private float respawnTime;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private int maxHp = 3;
+    [SerializeField] private int currentHp;
+    private Vector3 SavedPostion;
+    private Quaternion SavedRotaion;
+    private Vector3[] SavedBodyPos;
+    private Quaternion[] SavedBodyRotaion;
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    public void SliceStart()
-    {
-        foreach (Rigidbody2D OreBody in OreBodies)
+        OreBodies = new GameObject[target.childCount];
+        SavedBodyPos = new Vector3[OreBodies.Length];
+        SavedBodyRotaion = new Quaternion[OreBodies.Length];
+        currentHp = maxHp;
+        SavedPostion = transform.position;
+        SavedRotaion = transform.rotation;
+        for (int i = 0; i < target.childCount; i++)
         {
-            OreBody.simulated = true;
+            OreBodies[i] = target.GetChild(i).gameObject;
         }
-        StartCoroutine(nameof(RespawnCoroutine));
-        // Invoke(nameof(DestroyThis), 1f);
+        for (int i = 0; i < OreBodies.Length; i++)
+        {
+            SavedBodyPos[i] = OreBodies[i].transform.position;
+            SavedBodyRotaion[i] = OreBodies[i].transform.rotation;
+        }
     }
-
+    void ResetOre()
+    {
+        currentHp = maxHp;
+        transform.SetPositionAndRotation(SavedPostion, SavedRotaion);
+        for (int i = 0; i < OreBodies.Length; i++)
+        {
+            OreBodies[i].SetActive(true);
+            OreBodies[i].GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            OreBodies[i].transform.SetPositionAndRotation(SavedBodyPos[i], SavedBodyRotaion[i]);
+            OreBodies[i].SetActive(true);
+        }
+        GetComponent<CapsuleCollider2D>().enabled = true;
+    }
+    void DelOre()
+    {
+        GetComponent<CapsuleCollider2D>().enabled = false;
+    }
     void DropOre()
     {
         Instantiate(DropObject, transform.position, Quaternion.identity);
     }
-
-    void DestroyThis()
+    public void SliceStart()
     {
-        DropOre();
-        Destroy(gameObject);
+        if (currentHp - 1 <= 0)
+        {
+            StartCoroutine(nameof(RespawnCoroutine));
+            OreBodies[currentHp-1].SetActive(false);
+            currentHp--;
+        }
+        else
+        {
+            OreBodies[currentHp-1].GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            StartCoroutine(DelBody(OreBodies[currentHp-1]));
+            currentHp--;
+        }
+
+    }
+    private IEnumerator DelBody(GameObject _body)
+    {
+        yield return new WaitForSeconds(bodyRemainTime);
+        _body.SetActive(false);
     }
     private IEnumerator RespawnCoroutine()
     {
         if (respawnTime > 0f)
         {
             DropOre();
-            // 시각적 오브젝트와 콜라이더를 비활성화합니다.
-            // gameObject.SetActive(false);
-            // GetComponent<Collider2D>().enabled = false; // 상호작용을 막기 위해 콜라이더도 끔
-            // 설정된 시간만큼 대기
+            DelOre();
             yield return new WaitForSeconds(respawnTime);
-            Debug.Log("TEst");
-            ResetResource();
+            ResetOre();
+            Debug.Log("Test");
         }
-
         else
         {
             gameObject.SetActive(false);
         }
-    }
-    private void ResetResource()
-    {
-        // currentHp = maxHp;
-        // gameObject.SetActive(true);
-        // GetComponent<Collider2D>().enabled = true;
     }
 }
